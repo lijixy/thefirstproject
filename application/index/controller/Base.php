4b93743f2b2b79b45c74c71c0ceb8afd98c398c7
Mark <Mark@Mark-PC>
2018-10-15 08:43:17 +0800

    xiugai

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -7,6 +7,7 @@ use think\Controller;
 class Base extends Controller {
 
     function __construct() {
+    	//备用
         if (!session("adminID")) {
             parent::__construct();
             return view("login/index");
