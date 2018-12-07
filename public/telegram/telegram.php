93ac4e60318f86b04d4a9a6e5c0c6f0a7267625d
Mark <Mark@Mark-PC>
2018-12-07 15:02:57 +0800

    修改

--- public/telegram/telegram.php
+++ public/telegram/telegram.php
@@ -1,18 +1,5 @@
 <?php
-$bot_api_key = 'CHANGE HERE';
-/*
-function send_get($urlstring){
- $ch = curl_init();
- curl_setopt($ch, CURLOPT_URL, $urlstring);
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
- curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
- curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
- $result = curl_exec($ch);
- curl_close($ch); return $result;
-}
-*/
-
-
+$bot_api_key="727078481:AAFd56a-Za91ZN7e7V8-1i3FexMThfgstVM";
 function send_get_message($url){
 	$ch=curl_init();
 	curl_setopt($ch,CURLOPT_ERL,$url);
@@ -27,7 +14,11 @@ function send_get_message($url){
 
 $text = @$_GET["text"];
 $tgid = @$_GET["tgid"];
-print($text);
+if($text){
+	$url="https://api.telegram.org/bot".$bot_api_key."/sendMessage?chat_id=$tgid&text=".$text;
+	$get_send_result=send_get_message($url);
+	echo $get_send_result;
+}
 /*
 if($text){
  $url = "https://api.telegram.org/bot$bot_api_key/sendMessage?chat_id=$tgid&text=$text";
