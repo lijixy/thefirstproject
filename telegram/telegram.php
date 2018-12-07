f69d0386ee22ec9b775574bbb8f8707103f4f244
Mark <Mark@Mark-PC>
2018-12-07 13:59:05 +0800

    测试 电报

---
+++ telegram/telegram.php
@@ -0,0 +1,20 @@
+<?php
+$bot_api_key = 'CHANGE HERE';
+function send_get($urlstring){
+ $ch = curl_init();
+ curl_setopt($ch, CURLOPT_URL, $urlstring);
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+ curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+ $result = curl_exec($ch);
+ curl_close($ch); return $result;
+}
+$text = @$_GET["text"];
+$tgid = @$_GET["tgid"];
+if($text){
+ $url = "https://api.telegram.org/bot$bot_api_key/sendMessage?chat_id=$tgid&text=$text";
+ echo send_get($url);
+}else{
+ echo "Please Input";
+}
+?>
