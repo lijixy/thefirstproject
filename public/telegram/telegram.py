f237f90236c0289774d0f50f87f4a44bd0c11aad
Mark <Mark@Mark-PC>
2018-12-08 17:28:29 +0800

    修改

--- public/telegram/telegram.py
+++ public/telegram/telegram.py
@@ -3,7 +3,7 @@ import requests
 
 
 if __name__=="__main__":
-    url="https://api.telegram.org/bot609116902:AAHT0wmU_k1ICQ6s3aLQBpaoEUy4CDSXhcY/sendMessage?chat_id=613744501&text=你好吗？大帅"
+    url="https://api.telegram.org/bot609116902:AAHT0wmU_k1ICQ6s3aLQBpaoEUy4CDSXhcY/sendMessage?chat_id=613744501&text=welcome-new-link!"
     response=requests.requst("GET",url=url)
     print response.text
     print "link over!"
